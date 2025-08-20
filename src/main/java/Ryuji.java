af4be820fc79d3a9617d0b9bef9e15d0cf1c165e
Ryuji <ian.derp.lim@gmail.com>
2025-08-20 23:54:30 +0800

    level 0
    Changed the name of the chat bot and implemented an initial skeletal version of greeting and exiting

---
+++ src/main/java/Ryuji.java
@@ -0,0 +1,15 @@
+public class Ryuji {
+    public static void main(String[] args) {
+        String logo =  " ____               _ _ \n"
+                + "|  _ \\ _   _ _   _ (_|_)\n"
+                + "| |_) | | | | | | || | |\n"
+                + "|  _ <| |_| | |_| || | |\n"
+                + "|_| \\_\\\\__, |\\__,_|/ |_|\n"
+                + "       |___/     |__/   \n";
+        String line = "_______________________________________________________";
+        System.out.println(line + "\n" + logo);
+        System.out.println("Hello! I'm Ryuji\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
