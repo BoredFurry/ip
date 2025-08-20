6258953f038744674905479dc619eaeafb0dc473
Ryuji <ian.derp.lim@gmail.com>
2025-08-21 00:16:49 +0800

    level 1
    added echo feature. Ryuji can now read input and respond according to level 1 specifications

--- src/main/java/Ryuji.java
+++ src/main/java/Ryuji.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ryuji {
     public static void main(String[] args) {
         String logo =  " ____               _ _ \n"
@@ -10,6 +12,14 @@ public class Ryuji {
         System.out.println(line + "\n" + logo);
         System.out.println("Hello! I'm Ryuji\nWhat can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(line + "\nThis fox will always be here for you\n" + line);
+                break;
+            }
+            System.out.println(line + "\n" + command + "\n" + line);
+        }
     }
 }
